8876233af8e7d3abdea86307b87ca989dfa8d290
= <=>
2025-04-14 16:56:21 +0800

    js

--- ddt4c/javascript/function.js
+++ ddt4c/javascript/function.js
@@ -44,4 +44,48 @@ function orderFood() {
     info.innerHTML += "<h3>Thank you for your order!</h3>";
     priceInfo.innerHTML = "<strong>Total Price : RM " + price + "\n </strong>";
 
+
 }
+
+let btnKlik = document.getElementById('klik');
+
+btnKlik.onclick = function() {
+    alert('Button ditekan');
+}
+
+let txtWelcome = document.getElementById('welcome');
+
+txtWelcome.onmouseover = function() {
+    txtWelcome.innerText = "Selamat Datang";
+}
+
+txtWelcome.onmouseout = function() {
+    txtWelcome.innerText = "Welcome";
+}
+
+let kucing = document.getElementById('kucing');
+
+kucing.onmouseover = function() {
+    this.src = "https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg";
+
+    this.style.filter = "blur(0px)";
+    this.style.transform = "scale(2)"
+    this.style.transition = "transform 0.5s ease-in-out"
+
+}
+
+kucing.onmouseout = function() {
+    this.src = "https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg";
+
+    this.style.filter = "blur(2px)";
+    this.style.transform = "scale(1)"
+}
+
+function checkJabatan () {
+    let jabatan = document.getElementById('jabatan');
+    let selectedJabatan = jabatan.options[jabatan.selectedIndex].value;
+
+    alert("You have selected : " + selectedJabatan);
+    console.log(selectedJabatan);
+}
+
