6782d3eff30981c9c9f130d169c69e80fcda7a5c
Fareez-Laptop <fareez@psp.edu.my>
2025-04-15 22:29:47 +0800

    class activity with dit3b
    Signed-off-by: Fareez-Laptop <fareez@psp.edu.my>

--- DIT3B/javascript/function.js
+++ DIT3B/javascript/function.js
@@ -1,15 +1,15 @@
-function addition () {
+function addition() {
     let a = 5;
     let b = 3;
 
     console.log(a + b);
 }
 
-function substraction (a , b) {
+function substraction(a, b) {
     console.log(a - b);
 }
 
-function multiplication () {
+function multiplication() {
     let a = 2;
     let b = 5;
 
@@ -33,4 +33,19 @@ function countDown() {
         console.log("Countdown " + countdown);
         countdown++;
     }
+}
+
+function login() {
+
+    let username = prompt("Enter your username");
+    let password = prompt("Enter your password");
+
+    if ((username == "fareez" && password == "123456") || (username == "admin" && password == "123456")) {
+        console.log("Success");
+        alert("Welcome " + username);
+    } else {
+        console.log("Wrong username");
+        alert("Wrong username or password");
+    }
+
 }
